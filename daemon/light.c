6d96ea581bd70a05f7978ae689ab64816eeec73e
김민우 <minwoo3938@gmail.com>
2015-06-21 16:12:01 +0900

    light_sensor mqtt 구현/ light 센서 구현 완료.

--- daemon/light.c
+++ daemon/light.c
@@ -1,50 +1,51 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-
-#include <unistd.h>
 #include <pthread.h>
-
 #include <time.h>
-
-
 #include "daemon.h"
 #include "include/MQTTClient.h"
 
 /*
 func
 
+State machine 
+
+config->start->config   
+
+init 에서 mqtt 설정하기
 
 
+Topic 구독 방법 
+$ mosquitto_sub -t dev2/# 
+
+Topic 발행 방법 
+$ mosquitto_pub -t dev2/test -m "Message"
+
+설정 받는 토픽 
+dev2/config
 
 
+SET light_x 10
+SET light_y 10
+SET light_z 800
+
+Sprinkler 쪽에서 
 */
 void light_handler(int avg);
-
 
 void light_state();
 void light_config();
 void light_start();
-void light_running();
-void light_end();
 
-void light_func_test(int tic, int ticinterval, int avginterval);
-
-
+int mqtt_light_onReceiver(void *context, char *topicName, int topicLen, MQTTClient_message *message);
 
 enum LIGHT_STATE {
-
 	INIT,
 	CONFIG,
 	START,
-	RUNNING,
-	END,
-	TEST
 };
-
-
 static enum LIGHT_STATE state = INIT;
-
 
 enum SEASON {
 	SPRING,//(3~5)
@@ -52,133 +53,111 @@ enum SEASON {
 	FALL,//(9~11)
 	WINTER//(12,1,2)
 };
-
-
-
-
 static enum SEASON season = SPRING;
 
 enum NIGHTDAY {
 	NIGHT,
 	DAY
 };
-
 static enum NIGHTDAY nightday = NIGHT;
+
+enum BRIGHT {
+	DARKER,
+	BRIGHTER,
+	LITTLECHANE
+};
+static enum BRIGHT bright = LITTLECHANE;
+
 
 
 static char *dev_name = NULL;
 static int farm_fd = 0;
-
-
 static struct daemon_info_t info;
 
+static MQTTClient *mqtt = NULL;
 
-static int curavglux = 0;
+static char topic_light_config[32] = {0, };
+
 static int prevavglux = 0;
 static int deltalux = 0;
 
-static int count = 0;
-static int ticcnt = 0;
+static int light_x = 1;
+static int light_y = 10;
+static int light_z = 900;
+
+int mqtt_light_onReceiver(void *context, char *topicName, int topicLen, MQTTClient_message *message) {
+	// 서버로부터 설정에 대한 값이 들어왔을 경우
+	if( strncmp(topicName, topic_light_config, topicLen) == 0 ) {
+		char **setting = (char**)get_setting((char*)message->payload, message->payloadlen);
+		if( setting != NULL ) {
+
+			if( strcmp(setting[0], "light_x") == 0 ) {
+				printf("light_x: %s\n", setting[1]);
+				light_x = atoi(setting[1]);
+				destroy_setting(setting);
+			}
+			if( strcmp(setting[0], "light_y") == 0 ) {
+				printf("light_y: %s\n", setting[1]);
+				light_y = atoi(setting[1]);
+				destroy_setting(setting);
+			}
+			if( strcmp(setting[0], "light_z") == 0 ) {
+				printf("light_z: %s\n", setting[1]);
+				light_z = atoi(setting[1]);
+				destroy_setting(setting);
+			}
+		}
+	}
+	MQTTClient_freeMessage(&message);
+	MQTTClient_free(topicName);
+	return 1;
+}
 
 
 void *light_init(void *data) {
-
-
 	info = *((struct daemon_info_t*)data);
-
 	farm_fd = info.farm_fd;
 	dev_name = info.dev_name;
 
+	mqtt = (MQTTClient*)mqtt_create("light", mqtt_light_onReceiver);
+	mqtt_topic(dev_name, "light/config", topic_light_config);
+	mqtt_sub(mqtt, topic_light_config, 0);
 
-
-
-	state = TEST;
+	state = CONFIG;
 	while(1) {
 		light_state();
 		usleep(100000);
 	}
-
 }
 
-// tic = sec, ticinterval = sec, avginterval = min
-void light_func_test(int tic, int ticinterval, int avginterval){
-	
-	//ticcnt 만큼 호출하면 avginterval 만큼 지나있다.
 
-	// avginterval 마다 의 변화량의 틱을 알기위해
-	// 체크해야하는 avginterval / tic /ticinterval * 60
-
-	ticcnt = (int)(avginterval * 60 / tic / ticinterval);
-
-	//SPI_Regist~ : tic(sec)마다 lux값을 더한후 ticinterval(sec)으로 나눠서 평균값을 구함.
-	SPI_Register_Handler(farm_fd, 0, tic, ticinterval, light_handler);
-
-
-	//필요한것은 위에서 받아온 값을 가지고있다가 avginterval 이 지날때마다 값의 변화를 겟.
-
-
-	state = TEST;
-
-}
 
 
 void light_config(){
-
-
-
-
+	// 설정 변경.
+	state=START;
 }
+
 
 void light_start(){
-
+	SPI_Register_Handler(farm_fd, 0, &light_x, &light_y, light_handler);
+	state=CONFIG;
 }
-
-void light_running(){
-
-}
-
-void light_end(){
-
-}
-
 
 void light_state() {
 	switch(state) {
-		
-		case TEST:
-		printf("case:test\n");
-		//1초마다 습도를 가져오고
-		//10개의 샘플
-		// ==> 10초 동안의 평균
-		//1 분마다 변화량 체크
-		//parameter 
-		// sec, sec, min
-
-		light_func_test(1, 10, 1);
-		break;
-		
-
 		case CONFIG:
-
+		light_config();
 		break;
 
 		case START:
-
+		light_start();
 		break;
 
-		case RUNNING:
-
-		break;
-
-		case END:
-
+		default:
 		break;
 	}
-
-
 }
-
-
 
 
 void light_handler(int avg) {
@@ -189,7 +168,7 @@ void light_handler(int avg) {
   timer = time(NULL); // 현재 시각을 초 단위로 얻기
 
   t = localtime(&timer); // 초 단위의 시간을 분리하여 구조체에 넣기
-
+  t->tm_mon = t->tm_mon + 1;
 
   // Unix Time  timer // 1970-01-01 00:00:00 start.
   // year : t->tm_year + 1900);
@@ -198,8 +177,6 @@ void light_handler(int avg) {
 
   if(t->tm_mon>=3 && t->tm_mon<=5){
   	season = SPRING;
-  	printf("SPRING\n");
-
   	if(t->tm_hour <= 18 && t->tm_hour >= 5){
   		nightday = 1;
   	}else{
@@ -207,18 +184,13 @@ void light_handler(int avg) {
   	}
   } else if(t->tm_mon>=6 && t->tm_mon<=8){
   	season = SUMMER;
-  	printf("SUMMER\n");
-
   	if(t->tm_hour <= 19 && t->tm_hour >= 5){
   		nightday = 1;
   	}else{
   		nightday = 0;
   	}
-
   } else if(t->tm_mon>=9 && t->tm_mon<=11){
   	season = FALL;
-  	printf("FALL\n");
-
   	if(t->tm_hour <= 18 && t->tm_hour >= 5){
   		nightday = 1;
   	}else{
@@ -227,8 +199,6 @@ void light_handler(int avg) {
   	
   }  else if(t->tm_mon>=12 || t->tm_mon<=2){
   	season = WINTER;
-  	printf("WINTER\n");
-
   	if(t->tm_hour <= 17 && t->tm_hour >= 6){
   		nightday = 1;
   	}else{
@@ -237,45 +207,22 @@ void light_handler(int avg) {
 
   }
 
-
-  // 추가되면 사용할 변수. 
-  // curavglux = avg;
-
-  if(count%ticcnt==0){
-
-  	deltalux = prevavglux - avg;
-
-  	printf("deltalux = %d\n", deltalux);
-
-  	prevavglux = avg;
-  	count = 0;
-
-
-	// printf("count : %d\n", count);
+  deltalux = prevavglux - avg;
+  deltalux = -1 * deltalux;
+  prevavglux = avg;
 
   	//nightday==1  day
-  	if(nightday == 1){
-  		printf("day\n");
-  		if(deltalux > 900){
-  			printf("rainy\n");
-  		} else if(deltalux < -900) {
-  			printf("sunny\n");
-  		} else {
-  			printf("soso\n");
-  		}
-
-  	//nightday==0  night
-  	}else{
-  		printf("night\n");
-
+  if(nightday == 1){
+  	if(deltalux > light_z){
+  		bright = BRIGHTER
+  	} else if(deltalux < -1 * light_z) {
+  		bright=DARKER
+  	} else {
+  		bright=LITTLECHANE
   	}
+  	//nightday==0  night
+  }else{
+
   }
-
-
-
-  count++;
-
-	// printf("count : %d\n", count);
-  printf("Light Value Average: %d\n", avg);
 }
 
